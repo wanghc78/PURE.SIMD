3a231a2a0dcbdfa8eaf1301294e0cf1e386ddaa8
Avery Boyd <avb282@aol.com>
2013-10-03 01:52:28 -0500

    Updated YUV.c and MMM.c files for compatibility with SIMD instructions. Files run about 4x as fast. Avery Boyd

--- examples/week5/MMM.c
+++ examples/week5/MMM.c
@@ -1,8 +1,19 @@
-#define M_SIZE 256
+#include <stdio.h>
+#include <sys/time.h>
+#include <nmmintrin.h>
+
+#define M_SIZE 256 //256
 
 float A[M_SIZE*M_SIZE];
 float B[M_SIZE*M_SIZE];
 float C[M_SIZE*M_SIZE];
+
+static inline double get_sec()
+{
+  struct timeval tim;
+  gettimeofday(&tim, 0);
+  return tim.tv_sec+(tim.tv_usec/1000000.0);
+}
 
 /* Performs C = A(B^T) as a 2D matrix multiply */
 
@@ -53,12 +64,128 @@ void matrixTranspose(float A[M_SIZE*M_SIZE]) {
 	}
 }
 
-int main() {
-	int i;
+/* 
+ * Performs C = A(B^T) as a 2D matrix multiply 
+ * This is done using SIMD instructions
+*/
+void SIMDmatrixMultiply(float A[M_SIZE*M_SIZE], float B[M_SIZE*M_SIZE], float C[M_SIZE*M_SIZE]) {
+	__m128 tempA, tempB, sum;
 
-	for (i=0; i<10; i++) {
+	float total_sum[4];
+	int i,j,k;
+
+	for (i = 0; i < M_SIZE; i ++) {
+		for (j = 0; j < M_SIZE; j ++) {
+			sum = _mm_set1_ps(0.0f);
+			for (k = 0; k < M_SIZE; k += 4) {
+				tempA = _mm_loadu_ps(A + i*M_SIZE + k);
+				tempB = _mm_loadu_ps(B + j*M_SIZE + k);
+				sum = _mm_add_ps(sum,_mm_mul_ps(tempA,tempB));
+			}
+			_mm_storeu_ps(total_sum,sum);
+			C[i*M_SIZE+j] = total_sum[0] + total_sum[1] + total_sum[2] + total_sum[3];
+		}
+	}
+}
+
+/*
+ * Transposes A in place using SIMD instructions.
+ */
+void SIMDmatrixTranspose(float A[M_SIZE*M_SIZE]) {
+
+	void swap(int a, int b, int size) {
+		__m128 temp1, temp2;
+		int i,j;
+		for (i = 0; i < size; i ++) {
+			for (j = 0; j < size/4; j ++) {
+				float *swap1 = A + (a + i)*M_SIZE + b + size + j*4;
+				float *swap2 = A + (a + i + size)*M_SIZE + b + j*4;
+				temp1 = _mm_loadu_ps(swap1);
+				temp2 = _mm_loadu_ps(swap2);
+				_mm_storeu_ps(swap1,temp2);
+				_mm_storeu_ps(swap2,temp1);
+			}
+		}
+	}
+
+	void recursiveTranspose(int a, int b, int size) {
+		if (size > 4) {
+			int new_size = size/2;
+			swap(a,b,new_size);
+			recursiveTranspose(a,b,new_size);
+			recursiveTranspose(a + new_size,b,new_size);
+			recursiveTranspose(a,b + new_size,new_size);
+			recursiveTranspose(a + new_size,b + new_size,new_size);
+		}
+		else {
+			__m128 r1,r2,r3,r4,s1,s2,s3,s4;
+			float *swap1 = A + a*M_SIZE + b;
+			float *swap2 = A + (a + 1)*M_SIZE + b;
+			float *swap3 = A + (a + 2)*M_SIZE + b;
+			float *swap4 = A + (a + 3)*M_SIZE + b;
+			
+			r1 = _mm_loadu_ps(swap1);
+			r2 = _mm_loadu_ps(swap2);
+			r3 = _mm_loadu_ps(swap3);
+			r4 = _mm_loadu_ps(swap4);
+			
+			s1 = _mm_unpacklo_ps(r1,r3);
+			s2 = _mm_unpacklo_ps(r2,r4);
+			s3 = _mm_unpackhi_ps(r1,r3);
+			s4 = _mm_unpackhi_ps(r2,r4);
+			
+			r1 = _mm_unpacklo_ps(s1,s2);
+			r2 = _mm_unpackhi_ps(s1,s2);
+			r3 = _mm_unpacklo_ps(s3,s4);
+			r4 = _mm_unpackhi_ps(s3,s4);
+			
+			_mm_storeu_ps(swap1,r1);
+			_mm_storeu_ps(swap2,r2);
+			_mm_storeu_ps(swap3,r3);
+			_mm_storeu_ps(swap4,r4);
+		}
+	}
+
+	recursiveTranspose(0,0,M_SIZE);
+}
+
+void print_mat(float A[M_SIZE*M_SIZE]) {
+	int i,j;
+	for (i = 0; i < M_SIZE; i ++) {
+		for (j = 0; j < M_SIZE; j ++) {
+			printf("%d\t",(int)A[i*M_SIZE + j]);
+		}
+		printf("\n");
+	}
+}
+
+#define ITERATIONS 10
+
+int main() {
+	double start_time, end_time;
+	int i,j,k;
+	for (i = 0; i < M_SIZE; i ++) {
+		for (j = 0; j < M_SIZE; j ++) {
+			A[i*M_SIZE + j] = (rand() % 256) - 128;
+			B[i*M_SIZE + j] = (rand() % 256) - 128;
+		}
+	}
+
+	start_time = get_sec();
+	for(i = 0; i < ITERATIONS; i++) {
 		matrixTranspose(B);
 		matrixMultiply(A,B,C);
 	}
+	end_time = get_sec();
+	printf("Sequential version finished, time %f\n", (end_time-start_time));
+
+	start_time = get_sec();
+	for(i = 0; i < ITERATIONS; i++) {
+		matrixTranspose(B);
+		SIMDmatrixMultiply(A,B,C);
+	}
+	end_time = get_sec();
+	printf("SIMD version finished, time %f\n", (end_time-start_time));
+
 	return 0;
 }
--- examples/week5/YUV.c
+++ examples/week5/YUV.c
@@ -6,6 +6,10 @@
  *
  */
 
+#include <stdio.h>
+#include <sys/time.h>
+#include <nmmintrin.h>
+
 #define VECTOR_SIZE 4096
 
 short int R[VECTOR_SIZE];
@@ -14,6 +18,13 @@ short int B[VECTOR_SIZE];
 short int Y[VECTOR_SIZE];
 short int U[VECTOR_SIZE];
 short int V[VECTOR_SIZE];
+
+static inline double get_sec()
+{
+  struct timeval tim;
+  gettimeofday(&tim, 0);
+  return tim.tv_sec+(tim.tv_usec/1000000.0);
+}
 
 void convertRGBtoYUV() {
 
@@ -49,13 +60,124 @@ void convertYUVtoRGB() {
 	}
 }
 
-int main() {
+void AconvertRGBtoYUV() {
+	__m128i r,g,b,y,u,v;
+	__m128i rCo1 = _mm_set1_epi16((short)77);
+	__m128i gCo1 = _mm_set1_epi16((short)150);
+	__m128i bCo1 = _mm_set1_epi16((short)29);
+	__m128i rCo2 = _mm_set1_epi16((short)-43);
+	__m128i gCo2 = _mm_set1_epi16((short)-85);
+	__m128i bCo2 = _mm_set1_epi16((short)128);
+	__m128i rCo3 = _mm_set1_epi16((short)128);
+	__m128i gCo3 = _mm_set1_epi16((short)-107);
+	__m128i bCo3 = _mm_set1_epi16((short)-21);
+	__m128i cons1 = _mm_set1_epi16((short)32767);
+	__m128i cons2 = _mm_set1_epi16((short)256);
 
+	int remainder = VECTOR_SIZE % 8;
 	int i;
+	for (i = 0; i < VECTOR_SIZE - remainder; i += 8) {
+		r = _mm_loadu_si128((__m128i *) (R + i));
+		g = _mm_loadu_si128((__m128i *) (G + i));
+		b = _mm_loadu_si128((__m128i *) (B + i));
+		y = _mm_add_epi16(_mm_add_epi16(_mm_mullo_epi16(r,rCo1),_mm_mullo_epi16(g,gCo1)),_mm_mullo_epi16(b,bCo1));
+		u = _mm_add_epi16(_mm_add_epi16(_mm_add_epi16(_mm_mullo_epi16(r,rCo2),_mm_mullo_epi16(g,gCo2)),_mm_mullo_epi16(b,bCo2)),cons1);
+		v = _mm_add_epi16(_mm_add_epi16(_mm_add_epi16(_mm_mullo_epi16(r,rCo3),_mm_mullo_epi16(g,gCo3)),_mm_mullo_epi16(b,bCo3)),cons1);
+		y = _mm_add_epi16(y,cons2);
+		u = _mm_add_epi16(u,cons2);
+		v = _mm_add_epi16(v,cons2);
+		y = _mm_srai_epi16(y,8);
+		u = _mm_srai_epi16(u,8);
+		v = _mm_srai_epi16(v,8);
+		_mm_storeu_si128((__m128i *) (Y + i),y);
+		_mm_storeu_si128((__m128i *) (U + i),u);
+		_mm_storeu_si128((__m128i *) (V + i),v);
+	}
+	for (i; i < VECTOR_SIZE; i ++) {
+		Y[i] = (R[i]*77 + G[i]*150 + B[i]*29);
+		U[i] = (R[i]*-43 + G[i]*-85 + B[i]*128 + 32767);
+		V[i] = (R[i]*128 + G[i]*-107 + B[i]*-21 + 32767);
+		Y[i] = Y[i] + 256;
+		U[i] = U[i] + 256;
+		V[i] = V[i] + 256;
+		Y[i] = Y[i] >> 8;
+		U[i] = U[i] >> 8;
+		V[i] = V[i] >> 8;
+	}
+}
 
-	for (i=0; i<1000; i++) {
+void AconvertYUVtoRGB() {
+	__m128i r,g,b,y,u,v;
+	__m128i vCo1 = _mm_set1_epi16((short)360);
+	__m128i uCo1 = _mm_set1_epi16((short)88);
+	__m128i vCo2 = _mm_set1_epi16((short)184);
+	__m128i uCo2 = _mm_set1_epi16((short)455);
+	__m128i cons1 = _mm_set1_epi16((short)-128);
+	__m128i cons2 = _mm_set1_epi16((short)256);
+
+	int remainder = VECTOR_SIZE % 8;
+	int i;
+	for (i = 0; i < VECTOR_SIZE - remainder; i += 8) {
+		y = _mm_loadu_si128((__m128i *) (Y + i));
+		u = _mm_loadu_si128((__m128i *) (U + i));
+		v = _mm_loadu_si128((__m128i *) (V + i));
+		y = _mm_slli_epi16(y,8);
+		r = _mm_add_epi16(y,_mm_mullo_epi16(vCo1,_mm_add_epi16(v,cons1)));
+		g = _mm_sub_epi16(y,_mm_sub_epi16(_mm_mullo_epi16(uCo1,_mm_add_epi16(u,cons1)),_mm_mullo_epi16(vCo2,_mm_add_epi16(v,cons1))));
+		b = _mm_add_epi16(y,_mm_mullo_epi16(uCo2,_mm_add_epi16(u,cons1)));
+		r = _mm_add_epi16(r,cons2);
+		b = _mm_add_epi16(b,cons2);
+		g = _mm_add_epi16(g,cons2);
+		r = _mm_srai_epi16(r,8);
+		b = _mm_srai_epi16(b,8);
+		g = _mm_srai_epi16(g,8);
+		_mm_storeu_si128((__m128i *) (Y + i),y);
+		_mm_storeu_si128((__m128i *) (R + i),r);
+		_mm_storeu_si128((__m128i *) (G + i),g);
+		_mm_storeu_si128((__m128i *) (B + i),b);
+	}
+	for (i; i < VECTOR_SIZE; i ++) {
+		Y[i] = Y[i] << 8;
+		R[i] = (Y[i]+(360*(V[i]-128)));
+		G[i] = (Y[i]-(88*(U[i]-128) - (184*(V[i]-128))));
+		B[i] = (Y[i]+(455*(U[i]-128)));
+		R[i] = R[i] + 256;
+		G[i] = G[i] + 256;
+		B[i] = B[i] + 256;
+		R[i] = R[i] >> 8;
+		G[i] = G[i] >> 8;
+		B[i] = B[i] >> 8;
+	}
+}
+
+#define ITERATIONS 1000
+
+int main() {
+	int i;
+	double start_time;
+	double end_time;
+
+	for (i = 0; i < VECTOR_SIZE; i ++) {
+		R[i] = (short) (rand() % 256);
+		G[i] = (short) (rand() % 256);
+		B[i] = (short) (rand() % 256);
+	}
+
+	start_time = get_sec();
+	for(i = 0; i < ITERATIONS; i++) {
 		convertRGBtoYUV();
 		convertYUVtoRGB();
 	}
+	end_time = get_sec();
+	printf("Sequential version finished, time %f\n", (end_time-start_time));
+
+	start_time = get_sec();
+	for(i = 0; i < ITERATIONS; i++) {
+		AconvertRGBtoYUV();
+		AconvertYUVtoRGB();
+	}
+	end_time = get_sec();
+	printf("SIMD version finished, time %f\n", (end_time-start_time));
+
 	return 0;
 }
